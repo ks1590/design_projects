be46f636890f816c09b756726b1d6f63a4fe4193
kensuke suzuki <35k.0101@gmail.com>
2021-07-03 14:08:50 +0900

    touch slider

---
+++ touch_slider/script.js
@@ -0,0 +1,21 @@
+let swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+});
