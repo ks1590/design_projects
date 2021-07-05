929284dcd69acd930e6415807bea0b8281b1d790
kensuke suzuki <35k.0101@gmail.com>
2021-07-05 19:09:44 +0900

    clickアニメーション

--- Progress Steps/script.js
+++ Progress Steps/script.js
@@ -48,3 +48,18 @@ function update() {
     prev.disabled = false
   }
 }
+
+var animateButton = function (e) {
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  e.target.classList.add('animate');
+  setTimeout(function () {
+    e.target.classList.remove('animate');
+  }, 700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
