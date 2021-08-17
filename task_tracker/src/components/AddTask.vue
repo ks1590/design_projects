a6f0f83b75e0adc50c9874e275f5863f48322a35
kensuke suzuki <35k.0101@gmail.com>
2021-08-17 22:12:15 +0900

    add:タスク追加フォーム

---
+++ task_tracker/src/components/AddTask.vue
@@ -0,0 +1,59 @@
+<template>
+  <form @submit="onSubmit" class="add-form">
+    <div class="form-control">
+      <label>タスク名</label>
+      <input type="text" v-model="text" name="text" placeholder="Add Task" />
+    </div>
+    <div class="form-control">
+      <label>日時</label>
+      <input type="text" v-model="day" name="day" placeholder="Add Day & Time" />
+    </div>
+    <div class="form-control form-control-check">
+      <label>リマインダー</label>
+      <input type="checkbox" v-model="reminder" name="reminder" />
+    </div>
+
+    <input type="submit" value="Save Task" class="btn btn-block" />
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.add-form {
+  margin-bottom: 40px;
+}
+
+.form-contorl {
+  margin: 20px;
+}
+
+.form-control label {
+  display: block;
+}
+
+.form-control input {
+  width: 100%;
+  height: 40px;
+  margin: 5px;
+  padding: 3px 7px;
+  font-size: 17px;
+}
+
+.form-control-check {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.form-control-check label {
+  flex: 1;
+}
+
+.form-control-check input {
+  flex: 2;
+  height: 20px;
+}
+</style>
