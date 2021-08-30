f1187445fc690c9ef87c243a842a9522678d4652
kensuke suzuki <35k.0101@gmail.com>
2021-08-30 23:10:16 +0900

    add:poke API

---
+++ random_pokemon/app.js
@@ -0,0 +1,55 @@
+const colors = {
+  fire: '#FDDFDF',
+  grass: '#DEFDE0',
+  electric: '#FCF7DE',
+  water: 'DEF3FD',
+  ground: '#f4e7da',
+  rock: '#d5d5d4',
+  fairy: '#fceaff',
+  poison: '#ae98d7',
+  bug: '#f8d5a3',
+  dragon: '#97b3e6',
+  pcychic: '#eaeda1',
+  flying: 'F5F5F5',
+  fighting: '#E6E0D4',
+  normal: '#F5F5F5',
+}
+
+const main_types = Object.keys(colors)
+
+const app = Vue.createApp({
+  data() {
+    return {
+      pokemonName: "",
+      type: "",      
+      picture: "",
+    }
+  },
+  methods: {
+    async getPokemon() {
+      const min = 1
+      const max = 300
+      const pokemon_id = Math.floor(Math.random()*(max + 1 - min )) + min
+      
+
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon_id}`
+      const res = await fetch(url)
+      const pokemon = await res.json()
+      
+      const poke_types = pokemon.types.map(type => type.type.name)
+      const type = main_types.find(type => poke_types.indexOf(type) > -1)
+      const color = colors[type]
+      
+      this.pokemonName = pokemon.name
+      this.type = type
+      const pokemonEL = document.getElementById('img-container')      
+      pokemonEL.style.background = color
+
+      const pokemon_picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`
+      this.picture = pokemon_picture
+
+    }
+  }
+})
+
+app.mount("#app")
