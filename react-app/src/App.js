cd4a83f30d8fbc3f3ac062e11dc763d8bf6628ab
ks1590 <69412467+ks1590@users.noreply.github.com>
2022-07-02 22:52:33 +0900

    add: useState sample

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-    const title = 'NEW BLOG';
-
   return (
     <div className="App">
         <Navbar />
       <div className="content">
-          <h1>{ title }</h1>
+          <Home />
       </div>
     </div>
   );
