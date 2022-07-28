3c8c5bdf4ff4b8d60b7a310fb0d4252c2f35c7f7
ks1590 <69412467+ks1590@users.noreply.github.com>
2022-07-28 22:43:49 +0900

    clean: useeffect cleanup

--- react-app/src/Home.js
+++ react-app/src/Home.js
@@ -1,4 +1,3 @@
-import {useEffect, useState} from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
@@ -6,11 +5,11 @@ const Home = () => {
     const { data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
 
     return (
-      <div className="home">
-          { error && <div>{ error }</div> }
-          { isPending && <div>Loading...</div> }
-          { blogs && <BlogList blogs={ blogs } title="All Blogs"></BlogList> }
-      </div>
+        <div className="home">
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading...</div> }
+            { blogs && <BlogList blogs={ blogs } title="All Blogs"></BlogList> }
+        </div>
     );
 }
 
